098ef23ef0a982468530c2983bc90a7519295ce2
Kumar <dkumar3@yodlee.com>
2025-01-30 01:30:20 +0530

    update timeout

--- backend/db.js
+++ backend/db.js
@@ -3,8 +3,12 @@ const mongoose = require('mongoose');
 //const mongoURI = "mongodb://localhost:27017/eyecamp"
  
 const connectToMongo = () =>{
-    console.log(process.env.MONGO_URI);
-    mongoose.connect(process.env.MONGO_URI).then(
+    mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        connectTimeoutMS: 30000,  // Increase timeout to 30 seconds
+        socketTimeoutMS: 30000   // Increase socket timeout
+      }).then(
         ()=>{
             console.log("Conneted to mongo successfully");
         }
