7b1442e1c2ee70688d742159bd06c01ef8817785
Kumar <dkumar3@yodlee.com>
2025-01-30 01:20:31 +0530

    logger

--- backend/db.js
+++ backend/db.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 //const mongoURI = "mongodb://localhost:27017/eyecamp"
  
 const connectToMongo = () =>{
+    console.log(process.env.MONGO_URI);
     mongoose.connect(process.env.MONGO_URI).then(
         ()=>{
             console.log("Conneted to mongo successfully");
