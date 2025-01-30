89e03232175c627f858616c0ab3ccd67bc74f322
Kumar <dkumar3@yodlee.com>
2025-01-30 17:29:31 +0530

    missing file

--- frontend/src/component/PatientForm.js
+++ frontend/src/component/PatientForm.js
@@ -87,9 +87,6 @@ const PatientForm = (props) => {
         };
         fetchWithAlert(createPatient, headers)
             .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
                 return response.json()
             })
             .then(res => {
