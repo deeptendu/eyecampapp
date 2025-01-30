39a2c29f019372dc727c3d5620b8873193f53c68
Kumar <dkumar3@yodlee.com>
2025-01-30 11:20:58 +0530

    ghpages

--- frontend/src/ApiUrls.js
+++ frontend/src/ApiUrls.js
@@ -1,10 +1,11 @@
 
-export const loginUrl='http://localhost:5000/api/auth/login';
-export const userRegistration='http://localhost:5000/api/auth/createuser';
-export const getUser='http://localhost:5000/api/auth/getuser';
+export const host="https://eyecampapp.onrender.com";
+export const loginUrl='host/api/auth/login';
+export const userRegistration='host/api/auth/createuser';
+export const getUser='host/api/auth/getuser';
 
-export const createPatient='http://localhost:5000/api/createpatient';
-export const findPatient='http://localhost:5000/api/findpatient/';
-export const updatePatient='http://localhost:5000/api/updatepatient';
-export const getPatientList='http://localhost:5000/api/findpatientlist/';
+export const createPatient='host/api/createpatient';
+export const findPatient='host/api/findpatient/';
+export const updatePatient='host/api/updatepatient';
+export const getPatientList='host/api/findpatientlist/';
 
