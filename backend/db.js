c76f67f8464becca342de464b7a19a097683c998
Kumar <dkumar3@yodlee.com>
2025-01-30 11:26:04 +0530

    db

--- backend/db.js
+++ backend/db.js
@@ -4,8 +4,6 @@ const mongoose = require('mongoose');
  
 const connectToMongo = () =>{
     mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
         connectTimeoutMS: 30000,  // Increase timeout to 30 seconds
         socketTimeoutMS: 30000   // Increase socket timeout
       }).then(
